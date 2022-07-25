13e827ba54b0d486e08b89816683788f780ed0f8
Konstantin S <badmantegrow1@gmail.com>
2022-07-25 05:06:02 +0300

    парсер для работы часть 2

--- pars.py
+++ pars.py
@@ -2,23 +2,25 @@ import xlsxwriter
 from bs4 import BeautifulSoup
 
 out_xlsx = 'ColdCase.xlsx'
-# out_xlsx = 'Container02.xlsx'
 # out_xlsx = 'Sport.xlsx'
-# out_xlsx = 'Uma.xlsx'
 # out_xlsx = 'Repair.xlsx'
 # out_xlsx = 'Office.xlsx'
+# out_xlsx = 'ara.xlsx'
+# out_xlsx = 'Uma.xlsx'
 # out_xlsx = 'Sortavala.xlsx'
+# out_xlsx = 'Container02.xlsx'
 
 with open("ColdCase.html", encoding='utf-8') as fp:
-# with open("Container02.html", encoding='utf-8') as fp:
 # with open("Sport.html", encoding='utf-8') as fp:
-# with open("Uma.html", encoding='utf-8') as fp:
 # with open("Repair.html", encoding='utf-8') as fp:
 # with open("office.html", encoding='utf-8') as fp:
+# with open("ara.html", encoding='utf-8') as fp:
+# with open("Uma.html", encoding='utf-8') as fp:
 # with open("Sortavala.html", encoding='utf-8') as fp:
+# with open("Container02.html", encoding='utf-8') as fp:
     soup = BeautifulSoup(fp, "html.parser")
 link = soup.find_all('div', class_='_3SLg2')
-link1 = soup.find_all('div', class_='_1lBa-')
+# link1 = soup.find_all('div', class_='_1lBa-')
 
 
 d = []
@@ -29,23 +31,28 @@ for i in link:
     b = 'Проверить, если риг есть но он неработает, то запустить, если рига нет удалить из базы'
     c = 'Проверить наличие карты, если есть проверить ' \
         'работу карты, проверить питание карты (карт), заменить рейзер. В крайнем случае отправить карту на диагностику'
+    f = 'Понизить температуру, понизить разгон. Если не поможет смотреть на месте.'
     e = 'Перезагрузить, понизить разгон, если не помогло, убрать разгон. Проверить карту на месте.'
     damage1 = 'Не в сети'
     damage2 = 'Высокая температура'
     damage3 = 'Не майнит одна или несколько карт (красный квадратик)'
     damage4 = 'Низкий хешрейт, уснула одна или несколько карт'
     damage5 = 'В сети, но не майнит'
+    damage6 = 'Низкий коэффициент, битые шары'
+    damage7 = 'Низкий коэффициент и высокая температура'
     done = 'Повысил скорость куллера'
     done1 = 'Перезагрузил'
     done2 = 'Перезагрузил. Понизил разгон. На контроле.'
+    done3 = 'Понизил температуру, понизил разгон'
 
     comp = i.find('span', class_='_2eLpa').text
     comp1 = i.find('span', class_='dXZrd').text
+    # comp2 = i.find('span', class_='_1aEZL').text
     gpu = i.find_all('span', class_='_13cP- _1g0Tq')
     text = i.find('a', class_='ESArK').text.strip()
 
     if gpu:
-        print(f'{text} - {c}')
+        print(f'{text} - Не майнит одна или несколько карт (красный квадратик) - {c}')
         item = {
             'Номер рига': text,
             'Неисправность': damage3,
@@ -54,11 +61,18 @@ for i in link:
         }
         d.append(item)
 
-    elif comp == "Низкий коэффициент":
-        continue
+    elif comp == "Low hashrate":
+        print(f'{text} - Низкий хешрейт - {e}')
+        item = {
+            'Номер рига': text,
+            'Неисправность': damage4,
+            'Что требуется сделать': e,
+            'Что сделано': done2,
+        }
+        d.append(item)
 
-    elif comp == "Высокая температура":
-        print(f'{text} - {comp} - {a}')
+    elif comp == "High Temperature":
+        print(f'{text} - Высокая температура - {a}')
         item = {
             'Номер рига': text,
             'Неисправность': damage2,
@@ -77,8 +91,8 @@ for i in link:
         }
         d.append(item)
 
-    elif comp == "Не в сети":
-        print(f'{text} - {comp} - {b}')
+    elif comp == "Offline":
+        print(f'{text} - Не в сети - {b}')
         item = {
             'Номер рига': text,
             'Неисправность': damage1,
@@ -87,13 +101,23 @@ for i in link:
         }
         d.append(item)
 
-    elif comp == "Низкий хешрейт":
-        print(f'{text} - {comp} - {e}')
+    elif comp == "Low ratio":
+        print(f'{text} - Низкий коэффициент - {e}')
         item = {
             'Номер рига': text,
-            'Неисправность': damage4,
+            'Неисправность': damage6,
             'Что требуется сделать': e,
             'Что сделано': done2,
+        }
+        d.append(item)
+
+    elif comp == "Low ratioHigh Temperature":
+        print(f'{text} - Низкий коэффициент и высокая температура - {e}')
+        item = {
+            'Номер рига': text,
+            'Неисправность': damage7,
+            'Что требуется сделать': f,
+            'Что сделано': done3,
         }
         d.append(item)
 
